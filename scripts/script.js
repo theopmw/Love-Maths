49093c2a1388a86c3c4468d3ed130b27b4d62cc1
theopmw <theopmw@gmail.com>
2021-03-10 11:55:48 +0000

    Event listeners created for buttons to fire alert messages on button click

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,22 @@
-function runGame() {
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked the ${gameType} button`);
+            }
+        })
+    }
+})
+
+function runGame() { 
 
 }
 
@@ -28,4 +46,8 @@ function displaySubtractionQuestion() {
 
 function displayMultiplyQuestion() {
     
+}
+
+function displayDivisionQuestion() {
+
 }
